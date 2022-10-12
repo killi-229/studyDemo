822df75e2da16c8b13807b1734ef2f4f0d355de7
killi-299 <247459316@qq.com>
2022-10-12 17:03:42 +0800

    作业-实现useArray

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Screens from "./screens";
+import {TsReactTest} from "./try-use-array/TsReactTest";
+
 function App() {
   return (
       <div className="App">
-        <Screens/>
+        {/*<Screens/>*/}
+          <TsReactTest/>
       </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,16 @@
 import {useEffect, useState} from "react";
 
 // 判断是否零
-export const isFalsy = (v:unknown) => v === 0 ? false : !v
+export const isFalsy = (v: unknown) => v === 0 ? false : !v
 
 // 删除对象空值
-export const cleanObject = (o:object) => {
+export const cleanObject = (o: object) => {
     const ret = {...o}
 
     Object.keys(ret).forEach(k => {
         // @ts-ignore
         const v = ret[k]
-        if(!v){
+        if (!v) {
             // @ts-ignore
             delete ret[k]
         }
@@ -25,7 +25,7 @@ export const useMount = (callBack: () => void) => {
 }
 
 // 防抖: 触发高频事件后n秒内函数只会执行一次，如果n秒内高频事件再次被触发，则重新计算时间。
-export const useDebounce = <T>(value: T, delay?:number) => {
+export const useDebounce = <T>(value: T, delay?: number) => {
     const [debounceValue, setDebounceValue] = useState(value)
 
     useEffect(() => {
@@ -37,3 +37,32 @@ export const useDebounce = <T>(value: T, delay?:number) => {
 
     return debounceValue
 }
+
+
+// 练习
+// value, clear, removeIndex, add
+export const useArray = <T>(persons: T[])=> {
+    const [value, setValue] = useState(persons)
+
+    const clear = () => {
+        setValue([])
+    }
+    const removeIndex = (index: number) => {
+        const a = [...value]
+        a.splice(index, 1)
+        setValue(a)
+    }
+    const add = (item: T) => {
+        setValue([...value, item])
+    }
+
+
+    return {
+        value,
+        clear,
+        removeIndex,
+        add
+    }
+}
+
+
